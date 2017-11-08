4f7343fde203fb80eaed90d0aa99f54701c8c352
Adam Moisa <adammoisa@gmail.com>
2017-11-08 16:01:13 -0500

    slider in background

--- js/clean-blog.js
+++ js/clean-blog.js
@@ -37,18 +37,19 @@
         this.previousTop = currentTop;
       });
   }
-      var myIndex = 0;
-carousel();
+      var myIndex = 1;
 
-function carousel() {
-    var i;
-    var x = document.getElementsByClassName("mySlides");
-    for (i = 0; i < x.length; i++) {
-       x[i].style.display = "none";  
-    }
-    myIndex++;
-    if (myIndex > x.length) {myIndex = 1}    
-    x[myIndex-1].style.display = "block";  
-    setTimeout(carousel, 4000); // Change image every 2 seconds
-}
+      function carousel() {
+        var currentImg = 'url(img/school_header_imgs/hakotel/' + myIndex + '.jpeg)';
+        $('.masthead').css('background-image', currentImg);
+
+        if (myIndex === 20) myIndex = 0;
+
+        myIndex++;
+
+        setTimeout(carousel, 4000);
+      }
+
+      carousel();
+
 })(jQuery); // End of use strict
