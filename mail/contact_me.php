edabf71083ac1a6b479c36177f15a9c3b1a488d8
goldsteintzvi <goldsteintzvi@gmail.com>
2018-02-12 18:00:20 -0500

    first contact form working, played with Ask an Alum. Slideshow on HaKotel page stopped working

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -11,18 +11,18 @@ if (empty($_POST['name']) ||
 $name = strip_tags(htmlspecialchars($_POST['name']));
 $email_address = strip_tags(htmlspecialchars($_POST['email']));
 
-if (!empty($_POST['phone'])) {
-	$phone = strip_tags(htmlspecialchars($_POST['phone']));
-} else {
-	$phone = 'No number';
-}
+// if (!empty($_POST['phone'])) {
+//	$phone = strip_tags(htmlspecialchars($_POST['phone']));
+// } else {
+//	$phone = 'No number';
+// } 
 
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
 // Create the email and send the message
 $to = 'goldsteintzvi@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Contact from page: $name";
-$email_body = "You have received a new message from your website contact form.\n\n" . "Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+$email_body = "You have received a new message from your website contact form.\n\n" . "Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n$message";
 $headers = "From: $email_address\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";
 mail($to, $email_subject, $email_body, $headers);
